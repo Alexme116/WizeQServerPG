efb6defa8cd8911e12a0592b7f53f5e516e6795d
Alexme116 <alexmoncada16@hotmail.com>
2024-06-04 17:40:04 -0600

    db config rebase

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
-require('dotenv').config({ path: '../.env' })
-
 const Pool = require('pg').Pool
 
 const db = new Pool({
-    connectionString: process.env.POSTGRES_URL,
+    user: "alex",
+    password: "password",
+    host: "localhost",
+    port: 5432,
+    database: "wizeq"
 })
 
 module.exports = { db }
