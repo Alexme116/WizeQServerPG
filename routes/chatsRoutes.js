73b07bd6977017d28e50493bb1e9d4d937dee168
antoineganem <a01644024@tec.mx>
2024-06-13 01:15:01 -0600

    Almost finish RAG component

--- routes/chatsRoutes.js
+++ routes/chatsRoutes.js
@@ -6,5 +6,6 @@ router.get('/:id', chatsController.getChatsById);
 router.post('/', chatsController.createChat);
 router.delete('/:id', chatsController.deleteChat);
 router.put('/:id', chatsController.updateChat);
+router.post('/context',chatsController.getContextNearby);
 
 module.exports = router;
